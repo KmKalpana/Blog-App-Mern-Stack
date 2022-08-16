30b1f3b66f2325a53baf1b984954a21230fa8d54
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2022-08-16 13:59:31 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,22 @@ const mongoose=require("mongoose");
 const cors=require("cors");
 const router =require("./routes/userRoutes");
 const blogRouter=require("./routes/blogRoutes");
-app.use(cors());
+const path=require("path");
+/* require("dotenv").config();
+if(process.env.NODE_ENV==="production")
+{
+    app.use(express.static(path.join("frontend/build")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"fronend", "build", "index.html"));
+    })
+}*/
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use("/api/user",router);
 app.use("/api/blog",blogRouter);
 mongoose.connect("mongodb+srv://admin:itsFpvKO5KVyjNyh@cluster0.442zb.mongodb.net/BlogApp?retryWrites=true&w=majority").
-then(()=>app.listen(5000)).
+then(()=>app.listen(process.env.PORT || 5000)).
 then(()=>{console.log("Successfully Connected")}).
 catch((err)=>console.log(err));
