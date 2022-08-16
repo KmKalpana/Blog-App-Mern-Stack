d06522e5ab58d24ae8ed27b5fc2ac0d1b0283639
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2022-08-16 14:52:42 +0530

    deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import { Routes, Route, HashRouter } from 'react-router-dom';
-import './App.css';
-import Signup from './components/authentication/Signup';
-import Homepage from './HomePage';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Signup from './components/authentication/Signup'
+import AddBlog from './components/pages/AddBlog'
+import BlogDetail from './components/pages/BlogDetail'
 import Blogs from './components/pages/Blogs'
-import UserBlog from './components/pages/UserBlog';
-import BlogDetail from './components/pages/BlogDetail';
-import AddBlog from './components/pages/AddBlog';
-import {useSelector} from "react-redux";
+import UserBlog from './components/pages/UserBlog'
+import Homepage from './HomePage'
 function App() {
   // @ts-ignore
-  const isLoggedIn= useSelector((state)=>state.isLoggedIn)
-  console.log(isLoggedIn);
+  const isLoggedIn = useSelector((state) => state.isLoggedIn)
+  console.log(isLoggedIn)
   return (
     <React.Fragment>
-    <Homepage />
-    <main>
-     <HashRouter>
-          {!isLoggedIn ? (
-            <Route path="/auth" element={<Signup/>} />
-          ) : (
-            <>
-              <Route path="/blogs" element={<Blogs />} />
-              <Route path="/blogs/add" element={<AddBlog />} />
-              <Route path="/myBlogs" element={<UserBlog />} />
-              <Route path="/myBlogs/:id" element={<BlogDetail />} />{" "}
-            </>
-          )}
+      <Homepage />
+      <main>
+        <HashRouter>
+          <Routes>
+            {!isLoggedIn ? (
+              <Route path="/auth" element={<Signup />} />
+            ) : (
+              <>
+                <Route path="/blogs" element={<Blogs />} />
+                <Route path="/blogs/add" element={<AddBlog />} />
+                <Route path="/myBlogs" element={<UserBlog />} />
+                <Route path="/myBlogs/:id" element={<BlogDetail />} />{' '}
+              </>
+            )}
+          </Routes>
         </HashRouter>
-    </main>
+      </main>
     </React.Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
